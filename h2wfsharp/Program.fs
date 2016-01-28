353995c6737d4ef3f370a7f8a3d22bcd38aeaee2
Ryan Clarke <ryan@ryanclarke.net>
2016-01-28 11:24:56 -0500

    Slice code up into modules with 'smarter' types

--- h2wfsharp/Program.fs
+++ h2wfsharp/Program.fs
@@ -7,119 +7,121 @@ open FSharp.Data.JsonExtensions
 open System
 open System.IO
 
-type TokenProvider = JsonProvider<""" {"token":"guid string"} """>
-type ErrorProvider = JsonProvider<""" {"error":"http error"} """>
+module Client =
+    type TokenProvider = JsonProvider<""" {"token":"guid string"} """>
+    type ErrorProvider = JsonProvider<""" {"error":"http error"} """>
 
-type RunResponse =
-    | Response of FSharp.Data.HttpResponse
-    | Nothing of unit
+    type RunResponse =
+        | Response of FSharp.Data.HttpResponse
+        | Nothing of unit
 
-type EmailAndPassword = {
-    email: string
-    password: string
-}
+    type EmailAndPassword = {
+        email: string
+        password: string
+    }
 
-type Token = string
+    type Token = string
 
-type Credentials =
-    | UserCred of EmailAndPassword
-    | TokenCred of Token
+    type Credentials =
+        | UserCred of EmailAndPassword
+        | TokenCred of Token
 
-type Req = {
-    url: string
-    cred: Credentials
-}
+    type Req(url: string, credentials: Credentials) =
+        let credString cred =
+            match cred with
+            | UserCred({email=e; password=p}) -> sprintf "%s:%s" e p
+            | TokenCred(t) -> sprintf "%s:" t
 
-let credString cred =
-    match cred with
-    | UserCred({email=e; password=p}) -> sprintf "%s:%s" e p
-    | TokenCred(t) -> sprintf "%s:" t
+        let basic =
+            credentials
+            |> credString
+            |> System.Text.Encoding.UTF8.GetBytes
+            |> Convert.ToBase64String
+            |> sprintf "Basic %s"
 
-let basic cred =
-    cred
-    |> credString
-    |> System.Text.Encoding.UTF8.GetBytes
-    |> Convert.ToBase64String
-    |> sprintf "Basic %s"
+        member this.Url = url
+        member this.Cred = credentials
+        member this.BasicAuthorization = basic
 
-let h2wUrl endpoint = sprintf "https://h2w.cc/api/v1/%s" endpoint
+    let h2wUrl endpoint = sprintf "https://h2w.cc/api/v1/%s" endpoint
 
-let hitEndpoint req =
-    Http.Request(req.url,
-        headers = [ "X-H2W-Client-ID", "0"; "Authorization", basic req.cred ],
-        silentHttpErrors = true )
-    |> Response
+    let hitEndpoint (req:Req) =
+        Http.Request(req.Url,
+            headers = [ "X-H2W-Client-ID", "0"; "Authorization", req.BasicAuthorization ],
+            silentHttpErrors = true )
+        |> Response
 
-let helpText () = printfn "You're gonna need help to do this right."
+    let helpText () = printfn "You're gonna need help to do this right."
 
-let storeToken token =
-    File.WriteAllLines(".h2wfsharptoken", [token])
-    token
+    let storeToken token =
+        File.WriteAllLines(".h2wfsharptoken", [token])
+        token
 
-let getToken () = File.ReadAllLines(".h2wfsharptoken").[0] |> TokenCred
+    let getToken () = File.ReadAllLines(".h2wfsharptoken").[0] |> TokenCred
 
-let handleResponse run =
-    match run with
-    | Response resp -> 
-        printfn "URL:    %A" resp.ResponseUrl
-        printfn "STATUS: %A" resp.StatusCode
-        match resp.Body with
-        | Text(body) -> 
-            printfn "BODY:   %A" body
-            match resp.StatusCode with
-            | 200 ->
-                TokenProvider.Parse(body).Token
-                |> storeToken
-                |> printfn "TOKEN:  %A"
-            | _ ->
-                ErrorProvider.Parse(body).Error
-                |> printfn "ERROR:  %A"
-        | Binary(body) ->
-            match resp.StatusCode with
-            | 204 -> printfn "BODY:   <NO CONTENT>"
-            | _ -> printfn "BODY:   %A" body
-    | _ -> helpText()
+    let handleResponse run =
+        match run with
+        | Response resp -> 
+            printfn "URL:    %A" resp.ResponseUrl
+            printfn "STATUS: %A" resp.StatusCode
+            match resp.Body with
+            | Text(body) -> 
+                printfn "BODY:   %A" body
+                match resp.StatusCode with
+                | 200 ->
+                    TokenProvider.Parse(body).Token
+                    |> storeToken
+                    |> printfn "TOKEN:  %A"
+                | _ ->
+                    ErrorProvider.Parse(body).Error
+                    |> printfn "ERROR:  %A"
+            | Binary(body) ->
+                match resp.StatusCode with
+                | 204 -> printfn "BODY:   <NO CONTENT>"
+                | _ -> printfn "BODY:   %A" body
+        | _ -> helpText()
 
-let authCall args =
-    match args with
-    | email :: password :: x ->
-        { url=h2wUrl "auth/token"; cred=UserCred({ email=email; password=password }) }
-        |> hitEndpoint
-    | "verify" :: x ->
-        { url=h2wUrl "auth/token/verify"; cred=getToken() }
-        |> hitEndpoint
-    | _ -> Nothing()
+    let authCall args =
+        match args with
+        | email :: password :: x ->
+            Req(h2wUrl "auth/token", UserCred({ email=email; password=password }))
+            |> hitEndpoint
+        | "verify" :: x ->
+            Req(h2wUrl "auth/token/verify", getToken())
+            |> hitEndpoint
+        | _ -> Nothing()
 
-let call args =
-    match args with
-    | "auth" :: x -> authCall x
-    | "dashboard" :: x -> Nothing()
-    | _ -> Nothing()
+    let call args =
+        match args with
+        | "auth" :: x -> authCall x
+        | "dashboard" :: x -> Nothing()
+        | _ -> Nothing()
 
-let start args = args |> call |> handleResponse
+    let Start args = args |> call |> handleResponse
 
-let testCase args =
-    printfn "ARGS:   %A" args
-    start args
-    printfn "" 
+module Test =
+    let RunCase args =
+        printfn "ARGS:   %A" args
+        Client.Start args
+        printfn "" 
     
-let testBadCases () =
-    testCase ["garbage"]
-    testCase ["garbage"; "auth"]
+    let RunBadCases () =
+        RunCase ["garbage"]
+        RunCase ["garbage"; "auth"]
 
-let testAuthCases email password =
-    testCase ["auth"]
-    testCase ["auth"; email]
-    testCase ["auth"; email; "badpass"]
-    testCase ["auth"; "bademail"; password]
-    testCase ["auth"; email; password]
-    testCase ["auth"; "verify"]
+    let RunAuthCases email password =
+        RunCase ["auth"]
+        RunCase ["auth"; email]
+        RunCase ["auth"; email; "badpass"]
+        RunCase ["auth"; "bademail"; password]
+        RunCase ["auth"; email; password]
+        RunCase ["auth"; "verify"]
 
-let testAll email password =
-    testCase []
-    testBadCases()
-    testAuthCases email password
-    testCase ["dashboard"]
+    let RunAll email password =
+        RunCase []
+        RunBadCases()
+        RunAuthCases email password
+        RunCase ["dashboard"]
 
 [<EntryPoint>]
 let main argv =
@@ -127,11 +129,7 @@ let main argv =
     match args with
     | "test"::xs ->
         match xs with
-        | email::password::xss -> testAll email password
-        | _ -> testBadCases()
-    | _ -> start args
+        | email::password::xss -> Test.RunAll email password
+        | _ -> Test.RunBadCases()
+    | _ -> Client.Start args
     0 // return an integer exit code
-
-
-
-
