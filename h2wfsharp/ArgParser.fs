18cb4679e248425df4e324fbaa8a4f3086b99205
Ryan Clarke <ryan@ryanclarke.net>
2016-02-12 16:45:43 -0500

    Encapsulate flag parsing with nested function

--- h2wfsharp/ArgParser.fs
+++ h2wfsharp/ArgParser.fs
@@ -1,23 +1,22 @@
 ﻿namespace H2W
 
 module ArgParser =
-    open Client
     open Fiat
 
-    let rec private parseFlags args (fiat:Fiat) =
-        match args with
-        | ("-e" | "--email") :: email :: x -> 
-            parseFlags x {fiat with Email=email}
-        | ("-p" | "--password") :: password :: x ->
-            parseFlags x {fiat with Password=password}
-        | ("-t" | "--token") :: token :: x ->
-            parseFlags x {fiat with Token=token}
-        | ("-f" | "--file") :: file :: x ->
-            parseFlags x {fiat with File=file}
-        | ("-q" | "--quickstats") :: x -> parseFlags x {fiat with Quickstats=true}
-        | _ -> fiat
-
     let Parse args =
+        let rec parseFlags args (fiat:Fiat) =
+            match args with
+            | ("-e" | "--email") :: email :: x -> 
+                parseFlags x {fiat with Email=email}
+            | ("-p" | "--password") :: password :: x ->
+                parseFlags x {fiat with Password=password}
+            | ("-t" | "--token") :: token :: x ->
+                parseFlags x {fiat with Token=token}
+            | ("-f" | "--file") :: file :: x ->
+                parseFlags x {fiat with File=file}
+            | ("-q" | "--quickstats") :: x -> parseFlags x {fiat with Quickstats=true}
+            | _ -> fiat
+
         match args with
         | "auth" :: "verify" :: x ->
             DefaultFiat
